627219d67da58938237274dcb6ea32ab8a8a7f90
William Pinto <william.pinto@lambda3.com.br>
2018-02-27 16:59:57 -0300

    Add constructor to QnaPizzaDialog

--- FirstBot/FirstBot/Dialogs/QnaPizzaDialog.cs
+++ FirstBot/FirstBot/Dialogs/QnaPizzaDialog.cs
@@ -1,13 +1,21 @@
-﻿using System;
+﻿using Microsoft.Bot.Builder.CognitiveServices.QnAMaker;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 
 namespace FirstBot.Dialogs
 {
     [Serializable]
-    public class QnaPizzaDialog
+    public class QnaPizzaDialog : QnAMakerDialog
     {
+        //private string QnaSubscription = ConfigurationManager.AppSettings["QnaSubscriptionKey"];
+        //private string QnaKnowladgeBase = ConfigurationManager.AppSettings["QnaKnowledgeBaseId"];
 
+        public QnaPizzaDialog() : base(new QnAMakerService(new QnAMakerAttribute(ConfigurationManager.AppSettings["QnaSubscriptionKey"], ConfigurationManager.AppSettings["QnaKnowledgeBaseId"], "Não encontrei sua resposta", 0.5)))
+        {
+
+        }
     }
 }
